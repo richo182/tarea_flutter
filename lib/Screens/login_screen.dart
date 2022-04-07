06e01a620f3bc63bd43c63c198a2c69824030bf5
Ricardo Acosta Castillo <86420460+richo182@users.noreply.github.com>
2022-04-07 09:10:22 -0500

    Cambios de tarea 2

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -30,7 +30,7 @@ class _LoginScreenState extends State<LoginScreen> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _header(),
+            //_header(),
             const SizedBox(height: 20.0),
             _formulary()
           ],
@@ -56,12 +56,6 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             const SizedBox(height: 10.0),
-            const Text(
-              "Curso Flutter",
-              style: TextStyle(
-                  color: Colors.white
-              ),
-            ),
           ],
         ),
       ),
@@ -69,135 +63,148 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Widget _formulary(){
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 70.0),
-          child: Container(
-            decoration: const BoxDecoration(
+    return Container(
+      color: Colors.white10,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(height: MediaQuery.of(context).size.height * 0.15),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(100),
+            child: Image.asset(
+              'assets/images/AGP.jpg',
+              height: MediaQuery.of(context).size.height * 0.20,
+            ),
+          ),
+          const SizedBox(height: 40.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 70.0),
+            child: Container(
+              decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(
+                      color: Colors.white,
+                      width: 2.0,
+                    ),
+                  ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                child: TextField(
+                    controller: _controllerEmail,
+                    keyboardType: TextInputType.emailAddress,
+                    //textAlignVertical: TextAlignVertical.center,
+                    style: const TextStyle(
+                        color: Colors.grey,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 16.0,
+                    ),
+                    decoration: const InputDecoration(
+                      hintText: "Usuario",
+                      hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16.0,
+                      ),
+                      border: InputBorder.none,
+                      //icon: Icon(Icons.person)
+                    )
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 70.0),
+            child: Container(
+              decoration: BoxDecoration(
                 border: Border(
                   bottom: BorderSide(
                     color: Colors.white,
                     width: 2.0,
                   ),
                 ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50.0),
-              child: TextField(
-                  controller: _controllerEmail,
-                  keyboardType: TextInputType.emailAddress,
-                  //textAlignVertical: TextAlignVertical.center,
-                  style: const TextStyle(
-                      color: Colors.grey,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16.0,
-                  ),
-                  decoration: const InputDecoration(
-                    hintText: "Usuario",
-                    hintStyle: TextStyle(
-                        color: Colors.grey,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 16.0,
-                    ),
-                    border: InputBorder.none,
-                    //icon: Icon(Icons.person)
-                  )
               ),
-            ),
-          ),
-        ),
-        SizedBox(height: 15.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 70.0),
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(
-                  color: Colors.white,
-                  width: 2.0,
-                ),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: TextField(
-                        controller: _controllerPassword,
-                        keyboardType: TextInputType.emailAddress,
-                        obscureText: _protectPassword, // con esta propiedad volvemos el texto a asteriscos
-                        style: const TextStyle(
-                            color: Colors.black87,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16.0
-                        ),
-                        decoration: const InputDecoration(
-                          hintText: "Contraseña",
-                          hintStyle: TextStyle(
-                              color: Colors.grey,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                          controller: _controllerPassword,
+                          keyboardType: TextInputType.emailAddress,
+                          obscureText: _protectPassword, // con esta propiedad volvemos el texto a asteriscos
+                          style: const TextStyle(
+                              color: Colors.black87,
                               fontWeight: FontWeight.w600,
                               fontSize: 16.0
                           ),
-                          border: InputBorder.none,
-                          //icon: Icon(Icons.person)
-                        )
+                          decoration: const InputDecoration(
+                            hintText: "Contraseña",
+                            hintStyle: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w600,
+                                fontSize: 16.0
+                            ),
+                            border: InputBorder.none,
+                            //icon: Icon(Icons.person)
+                          )
+                      ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        _protectPassword = !_protectPassword;
-                      });
-                    },
-                    child: Icon(
-                        _protectPassword ? Icons.remove_red_eye : Icons.remove_red_eye_outlined
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-        SizedBox(height: 30.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 70.0),
-          child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  primary: Colors.white
-              ),
-              onPressed: (){
-
-              },
-              child: const Text(
-                'Login',
-                style: TextStyle(
-                    color: Colors.black45
+                    GestureDetector(
+                      onTap: (){
+                        setState(() {
+                          _protectPassword = !_protectPassword;
+                        });
+                      },
+                      child: Icon(
+                          _protectPassword ? Icons.remove_red_eye : Icons.remove_red_eye_outlined
+                      ),
+                    )
+                  ],
                 ),
-              )
-          ),
-        ),
-        SizedBox(height: 25.0),
-        Center(
-          child: Text(
-              '¿Olvidaste tu contraseña?',
-            style: TextStyle(
-                color: Colors.grey
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 25.0),
-        Center(
-          child: Text(
-            '© 2016-2022 REFACCIONARIAS AGP',
-            style: TextStyle(
-                color: Colors.grey
+          SizedBox(height: 30.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 70.0),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.white
+                ),
+                onPressed: (){
+
+                },
+                child: const Text(
+                  'Login',
+                  style: TextStyle(
+                      color: Colors.black45
+                  ),
+                )
             ),
           ),
-        ),
-      ],
+          SizedBox(height: 25.0),
+          Center(
+            child: Text(
+                '¿Olvidaste tu contraseña?',
+              style: TextStyle(
+                  color: Colors.grey
+              ),
+            ),
+          ),
+          SizedBox(height: 25.0),
+          Center(
+            child: Text(
+              '© 2016-2022 REFACCIONARIAS AGP',
+              style: TextStyle(
+                  color: Colors.grey
+              ),
+            ),
+          ),
+          SizedBox(height: 200.0),
+        ],
+      ),
     );
   }
 
